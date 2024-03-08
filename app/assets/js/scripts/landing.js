da526c6a9b2298655ae3e011e4a2e3e1e6f31e38
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-03-08 18:30:40 +0100

    ⚡️ First Push -Doing Things

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -149,7 +149,7 @@ function updateSelectedAccount(authUser){
             username = authUser.displayName
         }
         if(authUser.uuid != null){
-            document.getElementById('avatarContainer').style.backgroundImage = `url('https://mc-heads.net/body/${authUser.uuid}/right')`
+            document.getElementById('avatarContainer').style.backgroundImage = `url('https://starlightskins.lunareclipse.studio/skin-render/default/${authUser.uuid}/bust')`
         }
     }
     user_text.innerHTML = username
